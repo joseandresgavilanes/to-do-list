65ef34cff98f74a9282bbbec399ad921e30633f6
smiley_12_2 <joseandresgavilanes2012@gmail.com>
2023-01-04 21:01:51 -0500

    form

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import "./App.css";
+
+// custom components
+import CustomForm from "./components/CustomForm";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <div className="App"></div>;
+  return (
+    <div className="container">
+      <header>
+        <h1>My Task List</h1>
+      </header>
+      <CustomForm addTask={addTask} />
+    </div>
+  );
 }
 
 export default App;
